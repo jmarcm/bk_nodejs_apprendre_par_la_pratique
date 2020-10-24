4085a39ffefd9c1016d589fa94b35e670f3e0d90
Jean-Marc <mjean_marc@hotmail.com>
2020-10-24 14:29:04 +0200

    Update file

--- events/remove-class.js
+++ events/remove-class.js
@@ -1,12 +1,15 @@
 const EventEmitter = require("events");
 const { start } = require("repl");
 
+// extension de la classe EventEmitter
 class Car extends EventEmitter {
     constructor(name) {
         super();
         this.name = name;
     }
 
+    // encapsule un appel à la méthode emit()
+    // {disponible grâce à l'extension}
     start() {
         this.emit("action", this, "demarrer");
     }
